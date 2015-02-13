0f5108e2aad6fc10b49d1de21ef7e40fd70255fb
Peji <peji.faghihi@gmail.com>
2015-02-13 00:31:07 +0000

    Can rotate all sized images now

--- app/components/rotatable-image.js
+++ app/components/rotatable-image.js
@@ -12,39 +12,11 @@ export default Em.Component.extend({
     return 1;
   }),
 
-  aspectRatio: function() {
-    var height = this.get('height');
-    var width  = this.get('width');
-
-    return height / width;
-  }.property('height', 'width'),
-
-  parentAspectRatio: function() {
-    var height = this.get('parentHeight');
-    var width  = this.get('parentWidth');
-
-    return height / width;
-  }.property('parentHeight', 'parentWidth'),
-
-  isPortrait: function() {
-    return this.get('width') < this.get('height');
-  }.property(),
-
   style: function() {
     var degrees = this.get('degrees');
     var scale   = this.get('scale');
     return 'transform: rotate(' + degrees + 'deg) scale(' + scale + ');';
   }.property('degrees'),
-
-  src: function() {
-    //return 'test.jpg';
-    return 'noah.jpg';
-    //return 'noah2.jpg';
-    //return 'noah3.jpg';
-    //return 'noah4.jpg';
-    //return 'noah5.jpg';
-    //return 'noah6.jpg';
-  }.property(),
 
   height: function() {
     return this.$().height();
@@ -62,39 +34,51 @@ export default Em.Component.extend({
     return this.$().parent().width();
   }.property(),
 
-  click: function() {
-    var degrees = this.get('degrees');
-    if (degrees === 270) {
-      this.set('degrees', 0);
-    } else {
-      this.incrementProperty('degrees', 90);
-    }
-
-    var scale = this.get('scale');
-    var aspectRatio = this.get('parentAspectRatio');
-    var isPortrait = this.get('isPortrait');
-    var ratio = aspectRatio;
-
-    if (isPortrait) {
-      ratio = 1 / aspectRatio;
-    }
-
-    if (scale === 1) {
-      this.set('scale', ratio);
-    } else {
-      this.set('scale', 1);
-    }
-
+  swapWidthAndHeight: function() {
     var height = this.get('height');
     var width  = this.get('width');
-    var ph     = this.get('parentHeight');
-    var pw     = this.get('parentWidth');
+    this.set('height', width);
+    this.set('width', height);
+    width = this.get('width');
+    height = this.get('height');
+  },
 
-    console.log('IMAGE - width: ', width, ', height: ' + height);
-    console.log('PARENT - width: ', pw, ', height: ' + ph);
+  expandOrShrinkToFit: function() {
+    var height = this.get('height');
+    var width = this.get('width');
 
-    console.log('PORTRAIT?', this.get('isPortrait'));
+    var scale = this.get('scale');
 
-    console.log('ASPECT RATIO: ', this.get('aspectRatio'));
+    var virtualWidth = width * scale;
+    var virtualHeight = height * scale;
+    
+    var ph = this.get('parentHeight');
+    var pw = this.get('parentWidth');
+
+    if (virtualWidth > pw) {
+      scale = pw / width;
+      this.set('scale', scale);
+    } else if (virtualHeight > ph) {
+      scale = ph / height;
+      this.set('scale', scale);
+    } else if (virtualWidth < pw && virtualHeight < ph) {
+      var widthDiff = pw - width;
+      var heightDiff = ph - height;
+      if (widthDiff > heightDiff) {
+        scale = ph / height;
+        this.set('scale', scale);
+      } else {
+        scale = pw/width;
+        this.set('scale', scale);
+      }
+    }
+  },
+
+  click: function() {
+    this.incrementProperty('degrees', 90);
+    this.swapWidthAndHeight(); //Because javascript doesn't know we've rotated the image with CSS
+    this.expandOrShrinkToFit();
+    this.expandOrShrinkToFit(); //Just a hacky fix for smaller images. Can get rid if we expand on load.
   }
+
 });
